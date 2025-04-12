f8c02f5db505a104ac77abc4b437ff010b741d0c
Gowra Sivannarayana <sivannarayana210@gmail.com>
2025-04-12 12:40:34 +0530

    new rows

--- DailyCodes/class.js
+++ DailyCodes/class.js
@@ -12,3 +12,15 @@ class Product {
 }
 const Product1=new Product("Siva",250);
 Product1.dispalyProduct()
+
+class car {
+    constructor(name,year){
+        this.name=name;
+        this.year=year;
+    }
+}
+
+const car1=new car("nano",1999)
+console.log(car1.name);
+console.log(car1.year);
+
